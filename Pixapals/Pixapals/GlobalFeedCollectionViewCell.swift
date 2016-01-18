3ae159a8126f9982baf9f11fbfe30704a6e9d202
DARI <timalsina.bibek@gmail.com>
2016-01-18 14:30:21 +0545

    global feed working

---
+++ Pixapals/Pixapals/GlobalFeedCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalFeedCollectionViewCell.swift
+//  Pixapals
+//
+//  Created by DARI on 1/18/16.
+//  Copyright © 2016 com.pixpal. All rights reserved.
+//
+
+import UIKit
+
+class GlobalFeedCollectionViewCell: UICollectionViewCell {
+    
+}
