c6db9d5e50494abf0aa044080e0e033de59636bd
DARI <timalsina.bibek@gmail.com>
2016-02-05 09:41:26 +0545

    Automatic login fixed issue of double call of view will appear in tabbar controller

--- Pixapals/Pixapals/CustomTabBarController.swift
+++ Pixapals/Pixapals/CustomTabBarController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CustomTabBarController: UITabBarController {
-
+    var counter: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         //self.navigationItem.hidesBackButton = true
@@ -17,9 +17,9 @@ class CustomTabBarController: UITabBarController {
         //self.selectedViewController?.tabBarItem
         // Do any additional setup after loading the view.
         
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,28 +27,31 @@ class CustomTabBarController: UITabBarController {
     
     
     override func viewWillAppear(animated: Bool) {
-        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        let controller = storyBoard.instantiateViewControllerWithIdentifier("globalFeedVC")
-        
-        controller.title = "Personalized Feeds" //view = self.viewControllers?[0].view
-        
-        let icon1 = UITabBarItem(title: "", image: UIImage(named: "profile_menu"), selectedImage: nil)
-        icon1.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-        controller.tabBarItem = icon1
-        var controllers = self.viewControllers  //array of the root view controllers displayed by the tab bar interface
-        controllers?.insert(controller, atIndex: 1)
-        //controllers?.append(controller)
-        self.viewControllers = controllers
+        if !counter {
+            counter = !counter
+            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+            let controller = storyBoard.instantiateViewControllerWithIdentifier("globalFeedVC")
+            
+            controller.title = "Personalized Feeds" //view = self.viewControllers?[0].view
+            
+            let icon1 = UITabBarItem(title: "", image: UIImage(named: "profile_menu"), selectedImage: nil)
+            icon1.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+            controller.tabBarItem = icon1
+            var controllers = self.viewControllers  //array of the root view controllers displayed by the tab bar interface
+            controllers?.insert(controller, atIndex: 1)
+            //controllers?.append(controller)
+            self.viewControllers = controllers
+        }
     }
     
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
--- Pixapals/Pixapals/ViewControllers/ViewController.swift
+++ Pixapals/Pixapals/ViewControllers/ViewController.swift
@@ -23,13 +23,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
-//        let userInfo = UserDataStruct()
-//        
-//        if !userInfo.username.isEmpty {
-//            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-//            let vc = storyBoard.instantiateViewControllerWithIdentifier("tabView")
-//            self.navigationController?.pushViewController(vc, animated: true)
-//        }
+                let userInfo = UserDataStruct()
+        
+        if !userInfo.username.isEmpty {
+            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyBoard.instantiateViewControllerWithIdentifier("tabView")
+            self.navigationController?.pushViewController(vc, animated: false)
+        }
         
         //self.navigationController?.navigationBar.tintColor = UIColor(red:1.000, green:1.000, blue:1.000, alpha:1.00)
        // x.layer.borderColor
