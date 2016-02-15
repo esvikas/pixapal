87b9b64d29a56ea02e6c4c50d32466571306a322
DARI <timalsina.bibek@gmail.com>
2016-02-15 17:41:27 +0545

    notification design

---
+++ Pixapals/Pixapals/NotificationTableViewHeaderCell.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationTableViewHeaderCell.swift
+//  Pixapals
+//
+//  Created by DARI on 2/15/16.
+//  Copyright © 2016 com.pixpal. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewHeaderCell: UITableViewCell {
+
+    @IBOutlet weak var timeElapsedLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pixapals/Pixapals/NotificationViewController.swift
@@ -0,0 +1,46 @@
+//
+//  NotificationViewController.swift
+//  Pixapals
+//
+//  Created by DARI on 2/15/16.
+//  Copyright © 2016 com.pixpal. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+override func viewDidLoad() {
+        super.viewDidLoad()
+        //lbl.layer.cornerRadius = 8
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension NotificationViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        return tableView.dequeueReusableCellWithIdentifier("notificationHeaderCell", forIndexPath: indexPath)
+    }
+}
