d160fd115f5bde73a4140c550fcda48c585203fb
DARI <timalsina.bibek@gmail.com>
2016-02-16 08:54:09 +0545

    Aaakash update

--- Pixapals/Pixapals/NotificationViewController.swift
+++ Pixapals/Pixapals/NotificationViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import DZNEmptyDataSet
+
 
 class NotificationViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
 override func viewDidLoad() {
         super.viewDidLoad()
         //lbl.layer.cornerRadius = 8
         // Do any additional setup after loading the view.
+    
+    tableView.emptyDataSetDelegate=self
+    tableView.emptyDataSetSource=self
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,7 +40,7 @@ override func viewDidLoad() {
 
 }
 
-extension NotificationViewController: UITableViewDataSource {
+extension NotificationViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       return 1
     }
@@ -42,5 +48,37 @@ extension NotificationViewController: UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         return tableView.dequeueReusableCellWithIdentifier("notificationHeaderCell", forIndexPath: indexPath)
+    }
+}
+
+extension NotificationViewController : DZNEmptyDataSetDelegate, DZNEmptyDataSetSource {
+    
+    
+    func titleForEmptyDataSet(scrollView: UIScrollView!) -> NSAttributedString! {
+        let str = "Sorrry"
+        let attrs = [NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)]
+        return NSAttributedString(string: str, attributes: attrs)
+    }
+    
+    func descriptionForEmptyDataSet(scrollView: UIScrollView!) -> NSAttributedString! {
+        let str = "There are no posts to show"
+        let attrs = [NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleBody)]
+        return NSAttributedString(string: str, attributes: attrs)
+    }
+    
+    func imageForEmptyDataSet(scrollView: UIScrollView!) -> UIImage! {
+        return UIImage(named: "logo")
+    }
+    
+    //    func buttonTitleForEmptyDataSet(scrollView: UIScrollView!, forState state: UIControlState) -> NSAttributedString! {
+    //        let str = "More info"
+    //        let attrs = [NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleBody)]
+    //        return NSAttributedString(string: str, attributes: attrs)
+    //    }
+    
+    func emptyDataSetDidTapButton(scrollView: UIScrollView!) {
+        let ac = UIAlertController(title: "Info will be listed here", message: nil, preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "ok", style: .Default, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
     }
 }
